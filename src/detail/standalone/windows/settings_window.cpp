6e7b46aeca8dcb6d4057e12e2c0015863e709366
mthierman <mthierman@gmail.com>
2024-05-30 22:41:17 -0600

    win32 standalone: Create dropdowns/buttons for settings

--- src/detail/standalone/windows/settings_window.cpp
+++ src/detail/standalone/windows/settings_window.cpp
@@ -3,6 +3,12 @@
 #include "detail/standalone/standalone_details.h"
 #include <string>
 
+#define ID_COMBOBOX1 101
+#define ID_COMBOBOX2 102
+#define ID_COMBOBOX3 103
+#define ID_BUTTON1 104
+#define ID_BUTTON2 105
+
 namespace freeaudio::clap_wrapper::standalone::windows
 {
 void SettingsWindow::createWindow()
@@ -68,12 +74,31 @@ LRESULT CALLBACK SettingsWindow::wndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LP
   {
     switch (uMsg)
     {
+      case WM_CREATE:
+        return self->onCreate(hWnd, uMsg, wParam, lParam);
       case WM_CLOSE:
         return self->onClose(hWnd, uMsg, wParam, lParam);
     }
   }
 
   return ::DefWindowProcW(hWnd, uMsg, wParam, lParam);
+}
+
+int SettingsWindow::onCreate(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+  CreateWindow("COMBOBOX", NULL, WS_CHILD | WS_VISIBLE | CBS_DROPDOWNLIST, 10, 10, 150, 100, m_hwnd,
+               (HMENU)ID_COMBOBOX1, NULL, NULL);
+  CreateWindow("COMBOBOX", NULL, WS_CHILD | WS_VISIBLE | CBS_DROPDOWNLIST, 10, 50, 150, 100, m_hwnd,
+               (HMENU)ID_COMBOBOX2, NULL, NULL);
+  CreateWindow("COMBOBOX", NULL, WS_CHILD | WS_VISIBLE | CBS_DROPDOWNLIST, 10, 90, 150, 100, m_hwnd,
+               (HMENU)ID_COMBOBOX3, NULL, NULL);
+
+  CreateWindow("BUTTON", "Button 1", WS_CHILD | WS_VISIBLE, 10, 130, 80, 30, m_hwnd, (HMENU)ID_BUTTON1,
+               NULL, NULL);
+  CreateWindow("BUTTON", "Button 2", WS_CHILD | WS_VISIBLE, 100, 130, 80, 30, m_hwnd, (HMENU)ID_BUTTON2,
+               NULL, NULL);
+
+  return 0;
 }
 
 int SettingsWindow::onClose(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
