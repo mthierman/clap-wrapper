fa5e1723fafa30a9dcc48e8cb1f494b4ed6ef4b1
mthierman <mthierman@gmail.com>
2024-05-31 21:13:39 -0600

    Cleanup main

--- src/wrapasstandalone_win32.cpp
+++ src/wrapasstandalone_win32.cpp
@@ -1,6 +1,6 @@
 #include "detail/standalone/windows/app.h"
 
-int main(int argc, char* argv[])
+int main(int argc, char** argv)
 {
   return freeaudio::clap_wrapper::standalone::windows::app(argc, argv);
 }
